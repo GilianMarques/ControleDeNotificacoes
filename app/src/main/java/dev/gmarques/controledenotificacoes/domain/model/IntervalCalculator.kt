9a08684b008638f97a107fa10ceac0ba7deb0b05
Gilian Marques <gilian762@gmail.com>
2025-05-20 17:58:21 -0300

    Working on IntervalCalculator.kt

--- app/src/main/java/dev/gmarques/controledenotificacoes/domain/model/IntervalCalculator.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/domain/model/IntervalCalculator.kt
@@ -1,8 +1,13 @@
 package dev.gmarques.controledenotificacoes.domain.model
 
+import android.util.Log
+import dev.gmarques.controledenotificacoes.domain.model.TimeRangeExtensionFun.endIntervalFormatted
+import dev.gmarques.controledenotificacoes.domain.model.TimeRangeExtensionFun.startInMinutes
+import dev.gmarques.controledenotificacoes.domain.model.TimeRangeExtensionFun.startIntervalFormatted
 import dev.gmarques.controledenotificacoes.domain.model.enums.RuleType
 import dev.gmarques.controledenotificacoes.domain.model.enums.WeekDay
 import org.jetbrains.annotations.TestOnly
+import org.joda.time.DateTimeFieldType
 import org.joda.time.LocalDateTime
 import java.util.Calendar
 
@@ -21,9 +26,88 @@ class IntervalCalculator {
         if (rule.ruleType == RuleType.RESTRICTIVE) nextUnlockTimeFromNowRestricted(rule)
     }
 
-    private fun nextUnlockTimeFromNowRestricted(rule: Rule) {
+    private fun nextUnlockTimeFromNowRestricted(rule: Rule): Long {
 
-        getDaysRelativelySorted(rule.days, actualDayOfWeek)
+        val days = getDaysRelativelySorted(rule.days, actualDayOfWeek)
+        return iterateOverDays(days, rule)
+
+    }
+
+    private fun iterateOverDays(blockDays: List<Int>, rule: Rule): Long {
+
+        var todayLocalDateTime = LocalDateTime.now()
+
+        repeat(7) {
+
+            val weekDayInt = todayLocalDateTime.get(DateTimeFieldType.dayOfWeek())
+            // TODO: deve retornar o dia em questao as 00:00
+            if (weekDayInt !in blockDays) return todayLocalDateTime.toDate().time.also {
+                Log.d(
+                    "USUK",
+                    "IntervalCalculator.nextUnlockTimeFromNowRestricted:" + "data: ${todayLocalDateTime} de numero dia: $weekDayInt nao esta presente em ${blockDays}"
+                )
+            }
+
+            val nextUnlockTime = iterateOverTimeRanges(todayLocalDateTime, rule)
+            if (nextUnlockTime != null) return nextUnlockTime
+            // TODO: continuar aqui
+
+            /* dia de hoje consta na lista de dias da regra:
+               avalio os horarios e vejo se de agora até as 00:00 tem algum fim de intervalo de bloqueio
+                retorno hoje depois do fim do intervalo, se houver. Se nao o loop segue pelos proximos dias
+                 dia de hoje nao consta na lista de dias bloqueados  retorno o dia de hoje as 00:00 */
+
+            todayLocalDateTime = todayLocalDateTime.plusDays(1)
+
+        }
+    }
+
+    private fun iterateOverTimeRanges(
+        currentTime: LocalDateTime,
+        rule: Rule,
+    ): Long? {
+
+        val sortedTimeRanges = rule.timeRanges.sortedBy { it.startInMinutes() }
+
+        sortedTimeRanges.forEach { timeRange ->
+
+            Log.d(
+                "USUK",
+                "IntervalCalculator.iterateOverTimeRanges: checking ${timeRange.startIntervalFormatted()}:${timeRange.endIntervalFormatted()}"
+            )
+
+            if (rule.ruleType == RuleType.RESTRICTIVE) {
+
+                val timeRangeRelative = LocalDateTime(currentTime)
+                    .withHourOfDay(timeRange.endHour)
+                    .withMinuteOfHour(timeRange.endMinute)
+
+                if (timeRangeRelative.isAfter(currentTime)) return timeRangeRelative.plusMinutes(1).toDate().time.also {
+                    Log.d(
+                        "USUK",
+                        "IntervalCalculator.iterateOverTimeRanges: RESTRICTIVE next unlock period found: timeRangeRelative $timeRangeRelative currentTime $currentTime"
+                    )
+                }
+            }
+
+            if (rule.ruleType == RuleType.PERMISSIVE) {
+
+                val timeRangeRelative = LocalDateTime(currentTime)
+                    .withHourOfDay(timeRange.startHour)
+                    .withMinuteOfHour(timeRange.startMinute)
+
+                if (timeRangeRelative.isAfter(currentTime)) return timeRangeRelative.toDate().time.also {
+                    Log.d(
+                        "USUK",
+                        "IntervalCalculator.iterateOverTimeRanges: PERMISSIVE next unlock period found: timeRangeRelative $timeRangeRelative currentTime $currentTime"
+                    )
+                }
+            }
+
+
+        }
+        Log.d("USUK", "IntervalCalculator.iterateOverTimeRanges: no timerange found for next unlock: currentTime: $currentTime")
+        return null
     }
 
 
