f3a019dfbc432cd92f0dc871fcdd53aa1db3585a
Gilian Marques <gilian762@gmail.com>
2025-06-02 13:57:44 -0300

    This commit refactors the rule deletion process to ensure associated app notifications, including their cached PendingIntents and Bitmaps, are also removed.
    Key changes:
    
    - **DeleteRuleWithAppsUseCase:**
        - Modified to use `DeleteAllAppNotificationsUseCase` to remove notifications for all apps managed by the rule being deleted.
        - Utilizes `coroutineScope` and `async/awaitAll` to perform notification deletion concurrently for better performance.
    - **DeleteAllAppNotificationsUseCase:**
        - Updated to remove associated PendingIntents from `PendingIntentCache`.
        - Implemented logic to delete cached Bitmap files related to the notifications of the specified package ID from the application's cache directory.
    - **item_app_notification.xml:**
        - Adjusted constraints and dimensions for `iv_large_icon` and `tv_content` for better layout.
        - Changed `iv_large_icon` size to 48dp x 48dp.
        - Set `iv_large_icon` to be to the start of `tv_content`.
        - Adjusted padding for `iv_large_icon` and `tv_content`.

--- app/src/main/java/dev/gmarques/controledenotificacoes/domain/usecase/DeleteRuleWithAppsUseCase.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/domain/usecase/DeleteRuleWithAppsUseCase.kt
@@ -6,6 +6,11 @@ import dev.gmarques.controledenotificacoes.data.local.room.RoomDatabase
 import dev.gmarques.controledenotificacoes.domain.data.repository.ManagedAppRepository
 import dev.gmarques.controledenotificacoes.domain.data.repository.RuleRepository
 import dev.gmarques.controledenotificacoes.domain.model.Rule
+import dev.gmarques.controledenotificacoes.domain.usecase.app_notification.DeleteAllAppNotificationsUseCase
+import dev.gmarques.controledenotificacoes.domain.usecase.managed_apps.GetManagedAppsByRuleIdUseCase
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 /**
@@ -19,9 +24,14 @@ class DeleteRuleWithAppsUseCase @Inject constructor(
     private val roomDb: RoomDatabase,
     private val ruleRepository: RuleRepository,
     private val managedAppRepository: ManagedAppRepository,
+    private val deleteAllAppNotificationsUseCase: DeleteAllAppNotificationsUseCase,
+    private val getManagedAppsByRuleIdUseCase: GetManagedAppsByRuleIdUseCase,
 ) {
     suspend operator fun invoke(rule: Rule): Boolean {
         return try {
+
+            removeAppsNotifications(rule)
+
             roomDb.withTransaction {
                 managedAppRepository.deleteManagedAppsByRuleId(rule.id)
                 ruleRepository.deleteRule(rule)
@@ -31,5 +41,14 @@ class DeleteRuleWithAppsUseCase @Inject constructor(
             Log.e("USUK", "DeleteRuleWithAppsUseCase.invoke: Falha na transação: ${e.message}")
             false
         }
+    }
+
+    /**Usa o usecase adequado para remover as notificações (com pendingIntents
+     * e Bitmaps em cache) de cada app gerenciado pela regra
+     */
+    private suspend fun removeAppsNotifications(rule: Rule) = coroutineScope {
+        getManagedAppsByRuleIdUseCase(rule.id).map { app ->
+            async { deleteAllAppNotificationsUseCase(app.packageId) }
+        }.awaitAll()
     }
 }
--- app/src/main/java/dev/gmarques/controledenotificacoes/domain/usecase/app_notification/DeleteAllAppNotificationsUseCase.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/domain/usecase/app_notification/DeleteAllAppNotificationsUseCase.kt
@@ -1,8 +1,45 @@
 package dev.gmarques.controledenotificacoes.domain.usecase.app_notification
 
+import android.content.Context
+import android.util.Log
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.gmarques.controledenotificacoes.domain.data.repository.AppNotificationRepository
+import dev.gmarques.controledenotificacoes.framework.PendingIntentCache
 import javax.inject.Inject
 
-class DeleteAllAppNotificationsUseCase @Inject constructor(private val repository: AppNotificationRepository) {
-    suspend operator fun invoke(packageId: String) = repository.deleteAll(packageId)
+/**
+ * Remove todas as notificações de um determinado aplicativo  assim como as PendingIntents e Imagens em cache referentes à essas
+ * notificações
+ * */
+class DeleteAllAppNotificationsUseCase @Inject constructor(
+    private val repository: AppNotificationRepository,
+    @ApplicationContext private val context: Context,
+) {
+    suspend operator fun invoke(packageId: String) {
+        repository.deleteAll(packageId)
+        removePendingIntentsFromCache(packageId)
+        removeBitmapsFromCache(packageId)
+    }
+
+    private fun removeBitmapsFromCache(packageId: String) {
+        context.cacheDir.listFiles()?.forEach {
+            if (it.name.contains(packageId)) {
+                Log.d("USUK", "DeleteAllAppNotificationsUseCase.removeBitmapsFromCache: removing bitmap:  ${it.name}")
+                it.delete()
+            }
+        }
+    }
+
+    private fun removePendingIntentsFromCache(packageId: String) {
+        PendingIntentCache.cache.keys
+            .filter { it.contains(packageId) }
+            .forEach {
+                PendingIntentCache.cache.remove(it)
+                Log.d(
+                    "USUK",
+                    "DeleteAllAppNotificationsUseCase.removePendingIntentsFromCache: removing pendingIntent with key: ${it}"
+                )
+            }
+
+    }
 }
