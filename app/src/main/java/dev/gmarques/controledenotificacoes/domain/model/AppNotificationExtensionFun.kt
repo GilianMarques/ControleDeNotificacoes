f7ded6e2eb480df24a6b49439956b4a8f308a58c
Gilian Marques <gilian762@gmail.com>
2025-06-04 15:12:24 -0300

    refactor: Improve bitmap ID generation for caching
    This commit refines the bitmap ID generation logic for cached notification bitmaps.
    
    The `bitmapId()` extension function for `AppNotification` now incorporates the notification's title and content, in addition to the package ID. All non-alphanumeric characters are removed from this combined string before appending ".png".
    
    This change aims to prevent redundant bitmap storage. If a notification is updated without changes to its title or content (e.g., a music player notification during play/pause), the new bitmap will overwrite the existing one, conserving storage space.

--- app/src/main/java/dev/gmarques/controledenotificacoes/domain/model/AppNotificationExtensionFun.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/domain/model/AppNotificationExtensionFun.kt
@@ -36,8 +36,19 @@ object AppNotificationExtensionFun {
         return "${this.packageId}_${this.timestamp}"
     }
 
+    /**
+     * Cria uma id para os bitmaps armazenados em cache. Todos os caracteres exceto a-z, A-Z e 0-9 são removidos, incluindo espaços.
+     * A estrutura da id contem o nome de pacote da aplicação  pra que seja possivel filtrar todos os bitmaps de um determinado app
+     * e o uso do titulo e conteudo serve para impedir que uma determinada notificação salve um novo bitmap sempre que for atualizada.
+     * A ideia é que se a notificação nao sofrer alteraçao no titulo e/ou conteudo o bitmap dela sobrescrevera o bitmap que ja existe
+     * evitando um consumo desnecessario de armazenamento. Pense em um player de musica, toda vez que ocorre um play/pause a notificação
+     * é  atualizada e um novo bitmap é gerado mesmo que o bitmap seja o mesmo ja que a musica nao mudou. Entendeu?
+     * */
     fun AppNotification.bitmapId(): String {
-        return "${pendingIntentId()}.png"
+        return "${this.packageId}_${this.title}_${this.content}"
+            .replace(Regex("[^a-zA-Z0-9]"), "") +
+                ".png"
+
     }
 
 }
