9731194f80f55a727c132b7f6c754544aaedfb7a
Gilian Marques <gilian762@gmail.com>
2025-06-09 15:11:57 -0300

    feat: Add option to grant notification access in Settings
    This commit introduces a new option in the Settings fragment to allow users to grant notification listener permission.
    
    Key changes:
    
    - **SettingsFragment.kt:** Added `setupRequestNotificationAccessPermission` function that sets an `OnClickListener` on a new `TextView`. This listener launches an intent to `Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS`, guiding the user to the system settings page where they can grant the app permission to read notifications.
    - **fragment_settings.xml:** Added a `TextView` with the ID `tv_give_read_notification_permission` and styled as a settings menu item. It includes an icon and text prompting the user to "Permita ler as notificações" (Allow reading notifications).

--- app/src/main/java/dev/gmarques/controledenotificacoes/presentation/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/presentation/ui/fragments/settings/SettingsFragment.kt
@@ -1,7 +1,9 @@
 package dev.gmarques.controledenotificacoes.presentation.ui.fragments.settings
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -63,6 +65,7 @@ class SettingsFragment : MyFragment() {
         setupActionBar(binding.toolbar)
         setupResetHintsButton()
         setupResetBatteryOptimizationButton()
+        setupRequestNotificationAccessPermission()
         observeEvents()
         setupVersion()
     }
@@ -77,6 +80,16 @@ class SettingsFragment : MyFragment() {
         tvResetHints.setOnClickListener(AnimatedClickListener {
             viewModel.resetHints()
         })
+    }
+
+
+    private fun setupRequestNotificationAccessPermission() = with(binding) {
+
+        tvGiveReadNotificationPermission.setOnClickListener(AnimatedClickListener {
+            val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+            startActivity(intent)
+        })
+
     }
 
     private fun setupVersion() {
@@ -109,4 +122,6 @@ class SettingsFragment : MyFragment() {
             }
         }
     }
+
+
 }
